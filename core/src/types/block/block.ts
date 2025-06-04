090045e35a77e28fb9751a2f577222036bf238ab
Kid A <thisismeamir@outlook.com>
2025-06-04 22:37:12 +0330

    [delete] simple class to make instance of a block type. this is to be used inside the editor at runtime.

--- core/src/types/block/block.ts
+++ core/src/types/block/block.ts
@@ -3,13 +3,10 @@ import {BlockTypeRegistry} from "./blockTypeRegistry";
 /**
  * Actualization of a block in an editor
  */
-export class Block<
-    U,
-    T extends Record<string, any> = BlockTypeRegistry
-> {
+export class Block<U, T> {
     id: number
     raw: T
-    rendered: U
+    rendered?: U
 
     constructor(id: number, raw: T, rendered: U) {
         this.id = id
